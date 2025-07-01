a77669f790dc6bf4be9ab0807d4ee999d9424ce5
SriyansuDash <sriyansudash09@gmail.com>
2025-07-01 22:56:15 +0530

    Array.js

--- Javascript/Array.js
+++ Javascript/Array.js
@@ -1,31 +1,33 @@
-// let string = "This is javascript Webpage"; 
+let string = "This is javascript Webpage"; 
 
-// console.log(string[0]); 
-// console.log(string[1]);
-// console.log(string[2]);
-// console.log(string[3]);
-// console.log(string[4]);
-// console.log(string[5]);
+console.log(string[0]); 
+console.log(string[1]);
+console.log(string[2]);
+console.log(string[3]);
+console.log(string[4]);
+console.log(string[5]);
 
-// for(let i = 0;i<string.length;i++){
-//     console.log(string[i]);
-// }
+for(let i = 0;i<string.length;i++){
+    console.log(string[i]);
+}
 
-// cars = [['BMW','Audi','Suzuki'],
-//         ['Lambo', 'Pagani','Buggati'],
-//         ['Honda','Mahindra','Tata']];
-// // console.log("Car brand: "+ cars[0] + ", Price: BMW"); // Output: Car brand: BMW,
-// for(let i in cars) {
-//    for (let j=0 ;j < cars[i].length; j++) {
-//        console.log(`Car brand ${j+1}: ${cars[i][j]}`)
-//    }   
-// }
+cars = [['BMW','Audi','Suzuki'],
+        ['Lambo', 'Pagani','Buggati'],
+        ['Honda','Mahindra','Tata']];
+// console.log("Car brand: "+ cars[0] + ", Price: BMW"); // Output: Car brand: BMW,
+for(let i in cars) {
+   for (let j=0 ;j < cars[i].length; j++) {
+       console.log(`Car brand ${j+1}: ${cars[i][j]}`)
+   }   
+}
 
 top = ['R', 'G', 'Y']
 mid = ['W', 'R', 'O']
 bot = ['Y', 'W', 'G']
 face = [top, mid, bot]
 
-// for(let i = 0 ;i<face.length;i++){
+for(let i = 0 ;i<face.length;i++){
     console.log(face[1]);
-// }
+}
+
+
